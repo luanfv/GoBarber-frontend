00c11955c5630d94dc44e872c1ecb38b7fdff381
Luan <luanfrancavieira@gmail.com>
2020-07-29 20:44:16 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <GlobalStyle />
+    </>
   );
 }
 
