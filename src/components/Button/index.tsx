00780f3e282a58fec8bfc800cca23b43db06f677
Luan <luanfrancavieira@gmail.com>
2020-08-01 19:24:57 -0300

    isolated components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import { Container } from './style';
+
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = props => (
+    <Container>
+        <button type="button" { ...props }></button>
+    </Container>
+);
+
+export default Button;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Container, Content, Background } from './style';
 import logoImg from './../../assets/logo.svg';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import Input from './../../components/Input';
+import Button from './../../components/Button';
 
 const SignIn: React.FC = () => (
     <Container>
@@ -11,11 +13,11 @@ const SignIn: React.FC = () => (
             <form>
                 <h1>Faça seu logon</h1>
 
-                <input type="email" placeholder="E-mail" />
+                <Input icon={FiMail} name="email" type="email" placeholder="E-mail" />
 
-                <input type="password" placeholder="Senha" />
+                <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
 
-                <button type="submit">Entrar</button>
+                <Button type="submit">Entrar</Button>
 
                 <a href="forgot">Esqueci minha senha</a>
             </form>
