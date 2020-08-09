93ea2e6c2526dfd8ccfae87150b132c1ce78b92e
Luan <luanfrancavieira@gmail.com>
2020-08-09 17:19:46 -0300

    logout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+    const history = useHistory();
+
+    const handleLogout = useCallback(() => {
+        localStorage.removeItem('@GoBarber:token');
+        localStorage.removeItem('@GoBarber:user');
+
+        history.push('/');
+    }, [history]);
+
+    return (
+        <header>
+            <h1>Dashboard</h1>
+            <button type="button" onClick={handleLogout}>Logout</button>
+        </header>
+    );
+}
 
 export default Dashboard;
