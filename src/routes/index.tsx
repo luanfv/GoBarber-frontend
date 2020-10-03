ea687133b0ac96baa9d23f244b7beb3994251080
Luan França Vieira <luanfrancavieira@gmail.com>
2020-10-03 16:14:02 -0300

    reset password

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Dashboard from './../pages/Dashboard';
 import SignIn from './../pages/SignIn';
 import SignUp from './../pages/SignUp';
 import ForgotPassword from './../pages/ForgotPassword';
+import ResetPassword from './../pages/ResetPassword';
 
 import Route from './Route';
 
@@ -14,6 +15,7 @@ const Routes: React.FC = () => {
             <Route path="/" exact component={SignIn} />
             <Route path="/signup" exact component={SignUp} />
             <Route path="/forgot-password" exact component={ForgotPassword} />
+            <Route path="/reset-password" exact component={ResetPassword} />
 
             <Route path="/dashboard" exact component={Dashboard} isPrivate />
         </Switch>
